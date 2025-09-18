9d273d4648e96a9ab6504bcdf905f15d93e3a5b0
DevZaintech <web.zaintech@gmail.com>
2025-09-18 11:05:07 +0700

    pagination

--- resources/views/sales/lead/_table.blade.php
+++ resources/views/sales/lead/_table.blade.php
@@ -91,16 +91,20 @@
             @if ($lead->onFirstPage())
                 <span class="px-3 py-1 rounded bg-gray-200 text-gray-400 cursor-not-allowed">&laquo;</span>
             @else
-                <a href="{{ $lead->previousPageUrl() }}" 
+                <a href="{{ $lead->previousPageUrl() }}&{{ http_build_query(request()->query()) }}" 
                    class="px-3 py-1 rounded bg-blue-500 text-white hover:bg-blue-600">&laquo;</a>
             @endif
 
             {{-- Number Page --}}
             @foreach ($lead->getUrlRange(1, $lead->lastPage()) as $page => $url)
+                @php
+                    $fullUrl = $url . '&' . http_build_query(request()->query());
+                @endphp
+
                 @if ($page == $lead->currentPage())
                     <span class="px-3 py-1 rounded bg-blue-600 text-white font-bold">{{ $page }}</span>
                 @else
-                    <a href="{{ $url }}" 
+                    <a href="{{ $fullUrl }}" 
                        class="px-3 py-1 rounded bg-gray-200 text-gray-700 hover:bg-blue-500 hover:text-white">
                         {{ $page }}
                     </a>
@@ -109,7 +113,7 @@
 
             {{-- Tombol Next --}}
             @if ($lead->hasMorePages())
-                <a href="{{ $lead->nextPageUrl() }}" 
+                <a href="{{ $lead->nextPageUrl() }}&{{ http_build_query(request()->query()) }}" 
                    class="px-3 py-1 rounded bg-blue-500 text-white hover:bg-blue-600">&raquo;</a>
             @else
                 <span class="px-3 py-1 rounded bg-gray-200 text-gray-400 cursor-not-allowed">&raquo;</span>
@@ -117,4 +121,5 @@
         </nav>
     </div>
 @endif
+
 
